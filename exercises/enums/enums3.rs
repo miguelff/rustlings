b6ebb3d8f452e504d996f1c198e2f9ec0fa66801
Miguel Fernandez <miguel@mff.io>
2022-08-03 13:56:53 +0200

    Til strings1

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -1,11 +1,11 @@
 // enums3.rs
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
-
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    Move(Point),
+    Echo(String),
+    ChangeColor((u8, u8, u8)),
+    Quit,
 }
 
 struct Point {
@@ -37,7 +37,12 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
-        // TODO: create a match expression to process the different message variants
+        match message {
+            Message::Echo(s) => self.echo(s),
+            Message::Quit => self.quit(),
+            Message::Move(point) => self.move_position(point),
+            Message::ChangeColor((r, g, b)) => self.change_color((r, g, b)),
+        }
     }
 }
 
